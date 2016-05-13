18bdf8c71fdf688fadd532df9819d286feed9c01
Ritesh Singh <1124ritesh@gmail.com>
2016-05-13 22:52:50 +0530

    addmissionform

--- backend/views/student-master/_form.php
+++ backend/views/student-master/_form.php
@@ -2,6 +2,10 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\Arrayhelper;
+use backend\models\State;
+use backend\models\District;
+
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\StudentMaster */
@@ -12,84 +16,116 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+    <div class="row">
+        <div class="col-md-4">
 
-    <?= $form->field($model, 'adm_no')->textInput(['maxlength' => true]) ?>
+        <div class="panel panel-primary">
+          <div class="panel-heading">Student Personal Details</div>
+          <div class="panel-body">
+            <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'dob')->textInput() ?>
+            <?= $form->field($model, 'gender')->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'bloog_group')->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'stu_email')->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'photo')->textInput(['maxlength' => true]) ?>
+              </div>
+        </div>
+        
+        </div>
+        <div class="col-md-4">
 
-    <?= $form->field($model, 'dob')->textInput() ?>
+        <div class="panel panel-info">
+          <div class="panel-heading">Parent Details</div>
+          <div class="panel-body">
+            <?= $form->field($model, 'fname')->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'mname')->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'f_occupation')->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'm_occupation')->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'parent_contact')->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'parent_email')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'gender')->textInput(['maxlength' => true]) ?>
+          </div>
+          </div>
+        </div>
+          <div class="col-md-4">
+            <div class="panel panel-success">
+            <div class="panel-heading">Other Details</div>
+            <div class="panel-body">
+            <?= $form->field($model, 'class')->textInput() ?>
+            <?= $form->field($model, 'section')->textInput() ?>
+            <?= $form->field($model, 'transport_status')->dropDownList([ 'school' => 'School', 'self' => 'Self', ], ['prompt' => '']) ?>
+            <?= $form->field($model, 'catagory')->textInput() ?>
+            <?= $form->field($model, 'physically_disabled')->textInput() ?>
+            <?= $form->field($model, 'special_discount')->textInput() ?>
 
-    <?= $form->field($model, 'bloog_group')->textInput(['maxlength' => true]) ?>
+            </div>
+            </div>
+        </div>
+         
+    </div> 
+    <div class="row">
+    <div class="col-md-6">
+            <div class="panel panel-default">
+            <div class="panel-heading">Current Address Details</div>
+            <div class="panel-body">
+            <div class="row">
+            <div class="col-md-6">
+            <?= $form->field($model, 'cur_h_no')->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'cur_street_address')->textarea(['rows' => 8]) ?>
+            <?= $form->field($model, 'cur_area')->textInput(['maxlength' => true]) ?>
+            
+            </div>
+            <div class="col-md-6">
+            <?= $form->field($model, 'cur_state')->dropDownList(Arrayhelper::map(State::find()->all(),'id','name'),[
+                'prompt'=>'Please Select State'
+            ]) ?>
+            <?= $form->field($model, 'cur_district')->textInput() ?>
+            <?= $form->field($model, 'cur_po')->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'cur_pincode')->textInput() ?>
+            <?= $form->field($model, 'cur_contact')->textInput(['maxlength' => true]) ?>
+            
+            </div>
+            </div>
 
-    <?= $form->field($model, 'stu_email')->textInput(['maxlength' => true]) ?>
+            </div>
+            </div>
+        </div>
+         <div class="col-md-6">
+            <div class="panel panel-primary">
+            <div class="panel-heading">Permanent Address Details</div>
+            <div class="panel-body">
+            <div class="row">
+            <div class="col-md-6">
+            <?= $form->field($model, 'per_h_no')->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'per_street_address')->textarea(['rows' => 8]) ?>
+            <?= $form->field($model, 'per_area')->textInput(['maxlength' => true]) ?>
+            </div>
+            <div class="col-md-6">
+            <?= $form->field($model, 'per_state')->dropDownList(Arrayhelper::map(State::find()->all(),'id','name'),[
+                'prompt'=>'Please Select State'
+            ]) ?>
+            <?= $form->field($model, 'per_district')->textInput() ?>
+            <?= $form->field($model, 'per_po')->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'per_pincode')->textInput() ?>
+            <?= $form->field($model, 'per_contact')->textInput(['maxlength' => true]) ?>
+            </div>
 
-    <?= $form->field($model, 'photo')->textInput(['maxlength' => true]) ?>
+            </div>
+            </div>
+        </div>
 
-    <?= $form->field($model, 'fname')->textInput(['maxlength' => true]) ?>
+    
+    </div>
 
-    <?= $form->field($model, 'mname')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'f_occupation')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'm_occupation')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'parent_contact')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'parent_email')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'cur_h_no')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'cur_street_address')->textarea(['rows' => 6]) ?>
-
-    <?= $form->field($model, 'cur_area')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'cur_po')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'cur_district')->textInput() ?>
-
-    <?= $form->field($model, 'cur_state')->textInput() ?>
-
-    <?= $form->field($model, 'cur_pincode')->textInput() ?>
-
-    <?= $form->field($model, 'cur_contact')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'per_h_no')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'per_street_address')->textarea(['rows' => 6]) ?>
-
-    <?= $form->field($model, 'per_area')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'per_po')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'per_district')->textInput() ?>
-
-    <?= $form->field($model, 'per_state')->textInput() ?>
-
-    <?= $form->field($model, 'per_pincode')->textInput() ?>
-
-    <?= $form->field($model, 'per_contact')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'class')->textInput() ?>
-
-    <?= $form->field($model, 'section')->textInput() ?>
-
-    <?= $form->field($model, 'roll_no')->textInput() ?>
-
-    <?= $form->field($model, 'transport_status')->dropDownList([ 'school' => 'School', 'self' => 'Self', ], ['prompt' => '']) ?>
-
-    <?= $form->field($model, 'catagory')->textInput() ?>
-
-    <?= $form->field($model, 'physically_disabled')->textInput() ?>
-
-    <?= $form->field($model, 'special_discount')->textInput() ?>
-
-    <?= $form->field($model, 'created_at')->textInput() ?>
-
-    <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    <div class="row">
+        <div class="col-sm-5 col-sm-offset-5 ">
+            <div class="form-group">
+                <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Save') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-lg btn-success' : 'btn btn-primary']) ?>
+            </div>
+        </div>
     </div>
 
     <?php ActiveForm::end(); ?>
+</div>    
 
 </div>
--- backend/views/student-master/create.php
+++ backend/views/student-master/create.php
@@ -6,13 +6,13 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model backend\models\StudentMaster */
 
-$this->title = Yii::t('app', 'Create Student Master');
+$this->title = Yii::t('app', 'New Addmission');
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Student Masters'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="student-master-create">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    
 
     <?= $this->render('_form', [
         'model' => $model,
